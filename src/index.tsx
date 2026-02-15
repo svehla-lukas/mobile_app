bd9feaef21eb9f4d38f7ae9ea42bc98fe6c5ae8f
Svehla Lukas <105306348+svehla-lukas@users.noreply.github.com>
2026-02-15 11:23:23 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,31 +2,21 @@ import './index.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import Dictionary from './Dictionary'
 import DictionaryPeter from './DictionaryPeter'
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 
 serviceWorkerRegistration.register()
 
-const App = (): JSX.Element => {
-  const [mode, setMode] = useState<'main' | 'peter'>('main')
-
-  return (
-    <>
-      <div style={{ position: 'fixed', top: 10, right: 10 }}>
-        <button onClick={() => setMode('main')}>Main</button>
-        <button onClick={() => setMode('peter')}>Peter</button>
-      </div>
-
-      {mode === 'main' ? <Dictionary /> : <DictionaryPeter />}
-    </>
-  )
-}
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 
-root.render(<App />)
+root.render(
+  <>
+    <Dictionary />
+    <DictionaryPeter />
+  </>
+)
 
 reportWebVitals()
