b8f8a65006d906d71e6e242d00469ec4d7db7549
Svehla Lukas <105306348+svehla-lukas@users.noreply.github.com>
2026-02-15 11:21:05 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,32 @@
 import './index.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import Dictionary from './Dictionary'
-import React from 'react'
+import DictionaryPeter from './DictionaryPeter'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 
 serviceWorkerRegistration.register()
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const App = (): JSX.Element => {
+  const [mode, setMode] = useState<'main' | 'peter'>('main')
 
-root.render(
-  // <React.StrictMode>
-  <>
-    <Dictionary />
-  </>
-  // </React.StrictMode>
+  return (
+    <>
+      <div style={{ position: 'fixed', top: 10, right: 10 }}>
+        <button onClick={() => setMode('main')}>Main</button>
+        <button onClick={() => setMode('peter')}>Peter</button>
+      </div>
+
+      {mode === 'main' ? <Dictionary /> : <DictionaryPeter />}
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(<App />)
+
 reportWebVitals()
