2c890ca21039e16bd6572ae9bfe3b3c78bae4adb
Svehla Lukas <105306348+svehla-lukas@users.noreply.github.com>
2026-02-15 11:31:22 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,23 @@
 import './index.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import DictionaryPeter from './Dictionary'
-// import DictionaryPeter from './DictionaryPeter'
+import Dictionary from './Dictionary'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 
 serviceWorkerRegistration.register()
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
+  // <React.StrictMode>
   <>
     <Dictionary />
-    //  <DictionaryPeter />
   </>
+  // </React.StrictMode>
 )
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
